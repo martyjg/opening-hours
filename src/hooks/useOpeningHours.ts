fe870a2f55b7c784e9be37c616e572ec63f7209b
martyjg <martin.gormley@gorillasapp.com>
2023-02-02 14:26:00 +0100

    Fix formatting

--- src/hooks/useOpeningHours.ts
+++ src/hooks/useOpeningHours.ts
@@ -1,5 +1,7 @@
 import buildOpeningHours from '../helpers/buildOpeningHours/buildOpeningHours';
-import formatOpeningHours, { IOpeningValue } from '../helpers/formatOpeningHours/formatOpeningHours';
+import formatOpeningHours, {
+  IOpeningValue,
+} from '../helpers/formatOpeningHours/formatOpeningHours';
 import { useQuery } from '@tanstack/react-query';
 
 enum DayOfTheWeek {
@@ -15,19 +17,20 @@ enum DayOfTheWeek {
 export type OpeningHoursData = Partial<Record<DayOfTheWeek, IOpeningValue[]>>;
 
 // Take the data import and reformat it into an array with various time formats.
-export const deriveWeeklyOpeningHours = (data: OpeningHoursData) => Object.entries(data).map(
-  (dayOpeningHoursPairs) => ({
+export const deriveWeeklyOpeningHours = (data: OpeningHoursData) =>
+  Object.entries(data).map((dayOpeningHoursPairs) => ({
     name: dayOpeningHoursPairs[0],
-    openingHours: Array.isArray(dayOpeningHoursPairs[1]) ? dayOpeningHoursPairs[1].map((openingHour) => ({
-      ...buildOpeningHours(openingHour.value),
-      ...openingHour,
-    })) : [],
-  })
-);
+    openingHours: Array.isArray(dayOpeningHoursPairs[1])
+      ? dayOpeningHoursPairs[1].map((openingHour) => ({
+          ...buildOpeningHours(openingHour.value),
+          ...openingHour,
+        }))
+      : [],
+  }));
 
 const fetchOpeningHours = (): Promise<OpeningHoursData> => {
-  return fetch('data.json').then((response => response.json()))
-}
+  return fetch('data.json').then((response) => response.json());
+};
 
 const useOpeningHours = () => {
   const { isLoading, error, data } = useQuery({
